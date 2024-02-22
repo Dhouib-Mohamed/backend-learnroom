38af89c88fdf32616b52a34092eda43610390e74
nadamankai <nadamainkai@gmail.com>
2024-02-23 00:09:58 +0100

    commit test

--- src/classroom/classroom.controller.ts
+++ src/classroom/classroom.controller.ts
@@ -15,7 +15,6 @@ export class ClassroomController {
   @Get('')
   @UseGuards(RoleGuard())
   getByQuery( @Query('query') query: string, @GetUser() user: TokenUser) {
-    console.log("ggggg");
     return this.classroomService.searchByName(query, user);
   }
 
